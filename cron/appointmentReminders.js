148856344fdcd3b8af98e69178ac813e34609dd4
01 Jieddie <jietung02>
2024-04-16 11:32:57 +0800

    fix the cron job

--- cron/appointmentReminders.js
+++ cron/appointmentReminders.js
@@ -1,16 +1,19 @@
 const cron = require('node-cron');
-const moment = require('moment');
+const moment = require('moment-timezone');
 const { sendAppointmentReminder } = require('../services/sendEmailService');
 
 const startHour = parseInt(process.env.WORKING_HOUR);
 const closeHour = parseInt(process.env.CLOSING_HOUR);
 
+const scheduledTimes = [];
 
 // Cron job to run every 15 minutes between start and close hours
 cron.schedule(`*/15 ${startHour-1}-${closeHour - 1} * * *`, () => {
-    console.log('Running every 15 minutes between start and close hours');
-    console.log(moment().tz('Asia/Kuala_Lumpur').format('YYYY-MM-DD HH:mm'));
+    const scheduledTime = moment().tz('Asia/Kuala_Lumpur').format('YYYY-MM-DD HH:mm');
+    scheduledTimes.push(scheduledTime);
+    console.log(`Running every 15 minutes moment().tz('Asia/Kuala_Lumpur').format('YYYY-MM-DD HH:mm')`);
     sendAppointmentReminder();
+    console.log('All scheduled times:', scheduledTimes);
 }, {timezone: 'Asia/Kuala_Lumpur'});
 
 
