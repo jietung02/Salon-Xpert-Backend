4be34f739037423b16694d1c0356f70808cb56f7
01 Jieddie <jietung02>
2024-08-07 17:23:39 +0800

    remove 0.0.0.0

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { sendPaymentEmail } = require('./services/sendEmailService');
 const { sendAppointmentReminder } = require('./services/sendEmailService');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5;
 
 app.use(cors());
 app.use(express.json());
@@ -29,7 +29,7 @@ app.use('/feedback-management', feedbackManagementRouter)
 app.use('/service-management', serviceManagementRouter)
 
 try {
-    app.listen(port, '0.0.0.0',() => {
+    app.listen(port,() => {
         console.log(`Node ENV : ${process.env.NODE_ENV}`)
         console.log(`Server is running on port ${process.env.PORT}.`);
         console.log(`Connected to the database successfully!`);
